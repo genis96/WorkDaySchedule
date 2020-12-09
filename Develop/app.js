76eeacc98f4bf3d1bdb50e5c8bd3a78440f1418f
Genis Bardales <genis96@yahoo.com>
2020-12-09 01:21:49 -0500

    wrote js pseudo

---
+++ Develop/app.js
@@ -0,0 +1,10 @@
+/**
+ * grab selectors from html 
+ * use the moment.js format from the docs inside a jquery ready function. 
+ * it must display the time and the day 
+ * create an on "click" to listen for the users text input
+ *  must collect the values 
+ * use local storage (getItem)
+ * must change the color of the user input box 
+ * must addClass, and remove 
+ */
